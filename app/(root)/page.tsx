43253eab9fdf88748363c163769a09cadc03587e
Vladyslav <prostoandrei9@gmail.com>
2024-01-23 17:25:51 +0200

    clerk auth

---
+++ app/(root)/page.tsx
@@ -0,0 +1,8 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return <main>
+    <h1 className="text-4xl"></h1>
+  </main>;
+}
